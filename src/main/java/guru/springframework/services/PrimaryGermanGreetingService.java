f2a164fd1a9a64e1bdb8dbb6256fc411ce03957f
Nithin Chandran <nithinchandran.mec@gmail.com>
2018-10-21 11:15:29 +0530

    fixing the assignment

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -13,7 +13,10 @@ import org.springframework.stereotype.Service;
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
-
+    
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+    	this.greetingRepository = greetingRepository;
+    }
     @Override
     public String sayGreeting() {
         return greetingRepository.getGermanGreeting();
